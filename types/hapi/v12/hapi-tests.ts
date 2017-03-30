bb5daa437b9bbd932caecba86df8e52ed7896a35
Alexander James Phillips <ajp.global@gmail.com>
2017-03-30 11:05:35 +0100

    Replace mixed tabs and spaces to match hapi.js 4 spaces

--- types/hapi/v12/hapi-tests.ts
+++ types/hapi/v12/hapi-tests.ts
@@ -7,20 +7,20 @@ import * as Hapi from 'hapi';
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection(<Hapi.IServerConnectionOptions>{
-	host: "localhost",
-	port: 8000
+    host: "localhost",
+    port: 8000
 });
 
 // Add plugins
 var plugin: any = {
-	register: function (plugin: Object, options: Object, next: Function) {
-		next();
-	}
+    register: function (plugin: Object, options: Object, next: Function) {
+        next();
+    }
 };
 
 plugin.register.attributes = {
-	name: "test",
-	version: "1.0.0"
+    name: "test",
+    version: "1.0.0"
 };
 
 // optional options parameter
@@ -30,58 +30,58 @@ server.register({}, function (err) {});
 server.register({}, { select: 'api', routes: { prefix: '/prefix' } }, function (err) {});
 
 //server.pack.register(plugin, (err: Object) => {
-//	if (err) { throw err; }
+//    if (err) { throw err; }
 //});
 
 //server.pack.register([plugin], (err: Object) => {
-//	if (err) { throw err; }
+//    if (err) { throw err; }
 //});
 
 // Add server method
 var add = function (a: number, b: number, next: (err: any, result?: any, ttl?: number) => void) {
-	next(null, a + b);
+    next(null, a + b);
 };
 
 server.method("sum", add);//, { cache: { expiresIn: 2000 } });
 
 server.methods["sum"](4, 5, (err: any, result: any) => {
-	console.log(result);
+    console.log(result);
 });
 
 var addArray = function (array: Array<number>, next: (err: any, result?: any, ttl?: number) => void) {
-	var sum: number = 0;
-	array.forEach((item: number) => {
-		sum += item;
-	});
-	next(null, sum);
+    var sum: number = 0;
+    array.forEach((item: number) => {
+        sum += item;
+    });
+    next(null, sum);
 };
 
 server.method("sumObj", addArray, {
-	//cache: { expiresIn: 2000 },
-	generateKey: (array: Array<number>) => {
-		return array.join(',');
-	}
+    //cache: { expiresIn: 2000 },
+    generateKey: (array: Array<number>) => {
+        return array.join(',');
+    }
 });
 
 server.methods["sumObj"]([5, 6], (err: any, result: any) => {
-	console.log(result);
+    console.log(result);
 });
 
 // Add the route
 server.route({
-	method: 'GET',
-	path: '/hello',
-	handler: function (request: Hapi.Request, reply: Function) {
-		reply('hello world');
-	}
+    method: 'GET',
+    path: '/hello',
+    handler: function (request: Hapi.Request, reply: Function) {
+        reply('hello world');
+    }
 });
 
 server.route([{
-	method: 'GET',
-	path: '/hello2',
-	handler: function (request: Hapi.Request, reply: Function) {
-		reply('hello world2');
-	}
+    method: 'GET',
+    path: '/hello2',
+    handler: function (request: Hapi.Request, reply: Function) {
+        reply('hello world2');
+    }
 }]);
 
 // config.validate parameters should be optional
